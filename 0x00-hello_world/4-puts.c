e339e1d6f248d8d06c6e4a04ad2072ddece077f5
benjmarais <benjmarais@protonmail.com>
2023-03-16 17:14:13 +0200

    4x1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,8 @@
  * main - Print a line with the puts function
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts(""Programming is like building a multilingual puzzle");
 	return (0);
-}	
-	
-
-
-
+}
